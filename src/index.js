2d28928813b13c3c07ecb0e4d8fd06b3745d5574
syliu <syliu@clustertech.com>
2022-02-09 18:05:01 +0800

    feat: add webpack learn project

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { sum, print } from './js/utils.js'
+
+console.log('sum=', sum(10,20), print)
---
+++ src/js/utils.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => {
+    return a+b
+}
+
+const print = (value) => {
+    console.log('===>', print)
+}
+
+export {sum, print}
